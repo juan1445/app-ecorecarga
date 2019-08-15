e60ab6817256a6071e47a4d55b519c4e66722e50
juan1445 <juanzuleta1445@gmail.com>
2019-08-14 22:21:41 -0500

    Proyecto base para dividir en componentes

---
+++ src/components/HomeComponent.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <React.Fragment>
+                <div className="container-fluid">
+                    <h1>Hello Wordl</h1>
+                </div>
+            </React.Fragment>
+         );
+    }
+}
+ 
+export default Home;
