c404a45f99c70b499dd116383b4473689cbc25e6
pechuga22 <iefaz.santiagovalle@gmail.com>
2019-08-19 01:13:42 -0500

    actualizado

---
+++ src/components/FooterComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer=()=>{
+    return(
+        <footer className="py-4 bg-dark mt-4">
+            <div className="container">
+                <p className="m-0 text-center text-white">2019 Todos los derechos reservados &copy; ecorecarga.com</p>
+            </div>
+            </footer>
+    );
+}
+export default Footer;
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import NavBar from "./NavBarComponent";
+import Footer from "./FooterComponent";
 
 const Layout = props => {
   return (
     <React.Fragment>
       <NavBar />
       {props.children}
+      <Footer/>
     </React.Fragment>
   );
 };
